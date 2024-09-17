e1666fa20986da5f220b313e71324e43e05746ec
MaxjoseC <Maxjose17@gmail.com>
2024-09-17 11:51:17 -0400

    git ignore creado y mysql añadido

--- Rapi/Rapi/settings.py
+++ Rapi/Rapi/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Rapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'Emajose12',
+        'HOST': 'localhost',  
+        'PORT': '3306',
     }
 }
 
